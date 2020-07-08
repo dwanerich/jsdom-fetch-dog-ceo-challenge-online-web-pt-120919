633bb67a645e0639d8856dd7174134c35e36634a
dwanerich <dwanerich@gmail.com>
2020-07-07 23:47:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchImages () {
+    return fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(json => console.log(json));
+
+}
+
+function renderImages(images) {
+    console.log(images)
+    const main = document.querySelector('main')
+
+
+
+    images.forEach(message => {
+        const img = document.createElement('img')
+        img.src = message
+        main.appendChild(img)
+    })
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetchImages()
+})
